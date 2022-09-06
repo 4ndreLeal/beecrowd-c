25d9c0824cf98fd3aa7c3fee2246539c3b6f333e
André <110499972+4ndreLeal@users.noreply.github.com>
2022-09-06 19:59:28 -0300

    average 2

---
+++ 1006.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    double A, B, C, average;
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+    scanf("%lf", &C);
+    average = (A * 2 + B * 3 + C * 5) / 10;
+    printf("MEDIA = %.1lf\n", average);
+    return 0;
+}
