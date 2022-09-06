53b300861d1cf9dab8872ed40e55f909e961321f
André <110499972+4ndreLeal@users.noreply.github.com>
2022-09-06 19:45:06 -0300

    simple product

---
+++ 1004.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int value1, value2, prod;
+    scanf("%d", &value1);
+    scanf("%d", &value2);
+    prod = value1 * value2;
+    printf("PROD = %d\n", prod);
+    return 0;
+}
